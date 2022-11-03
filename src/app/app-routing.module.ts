7b67b50934788a5604897a66e3c94884e2b06807
akekachai <akekachaiaj@gmail.com>
2022-11-03 12:47:02 +0700

    v5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,10 +10,10 @@ const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: '', component: AppLayoutComponent, canActivate:[AuthGuard],
     children: [
-    //  { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+      { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
       { path: '',loadChildren:() => import('./demo/components/person/person.module').then(m => m.PersonModule) },
-     //  { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
-     //{ path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
+      { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
+     { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
        { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
        { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
        { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
