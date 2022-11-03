f772d1ad03fd83d2531c81da50465aebf0bda4f2
akekachai <akekachaiaj@gmail.com>
2022-11-03 11:41:00 +0700

    V031102022_1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'badminton_v1'`, () => {
+  it(`should have as title 'badminton'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('badminton_v1');
+    expect(app.title).toEqual('badminton');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('badminton_v1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('badminton app is running!');
   });
 });
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+const AUTH_API = 'http://45.136.255.182:5003/api/Persons';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) {}
+  login(userid: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + '/authenticate',
+      {
+        userid,
+        password,
+      },
+      httpOptions
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + '',
+      {
+        username,
+        email,
+        password,
+      },
+      httpOptions
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(AUTH_API + 'signout', { }, httpOptions);
+  }
+
+  refreshToken(token: string) {
+    console.log('7');
+    return this.http.post(AUTH_API + 'refresh-token', {
+      refreshToken: token
+    }, httpOptions);
+  }
+}
